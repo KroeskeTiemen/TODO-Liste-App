5eb88813e29ade877d4f252a8ab977d222b93228
Tiemen Kroeske <tkroeske@sievers-group.com>
2026-04-08 12:05:30 +0200

    Implement POST (Add List Method)

--- flaskServer.py
+++ flaskServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import uuid
 
 todo_lists = {}
@@ -13,5 +13,19 @@ def create_uuid():
 # Fehlerantwort
 def error(message, code):
     return jsonify({"message": message}), code
+
+#Add List Method
+@app.route("/todo-list", methods=["POST"])
+def create_list():
+    data = request.get_json()
+    if not data or "name" not in data:
+        return error("Invalid input", 406)
+
+    list_id = create_uuid()
+    todo_lists[list_id] = {
+        "id": list_id,
+        "name": data["name"]
+    }
+    return jsonify(todo_lists[list_id]), 201
 
 app.run(debug=True)
